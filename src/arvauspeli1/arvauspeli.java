566a72ce098549d7b854da272f23ed3894444313
Manmi12-star <142732960+Manmi12-star@users.noreply.github.com>
2023-09-14 13:14:00 +0300

    Initial commit

---
+++ src/arvauspeli1/arvauspeli.java
@@ -0,0 +1,44 @@
+package arvauspeli1;
+
+import java.util.Scanner;
+
+public class arvauspeli {
+
+	public static void main(String[] args) {
+
+		
+		Scanner in = new Scanner(System.in);
+
+		String nimi;
+		int guess = 0;
+		String stop = "End";
+
+		System.out.println("What is my name");
+
+		do
+		{
+		nimi = in.nextLine();
+//		if (stop.equals(nimi)) {
+//			System.out.println("You guessed " + guess + " times.");
+//			guess--;
+//			}
+		
+		guess++;
+		if(nimi.equals("Anastasia"))
+		{
+		System.out.println("You guessed right.");
+
+		break;
+		}
+
+		if (!nimi.equals(stop))
+		{
+		System.out.println("You can guess again or write End to end the game)");
+		
+		}
+		}
+		while(!nimi.equals("End"));
+
+	}
+
+}
